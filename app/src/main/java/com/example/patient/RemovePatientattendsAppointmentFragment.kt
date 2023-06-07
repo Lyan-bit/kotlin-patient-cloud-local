0299fbb22267b6bacb518e547d6d228460e92bab
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-07 14:01:58 +0100

    Update RemovePatientattendsAppointmentFragment.kt

--- app/src/main/java/com/example/patient/RemovePatientattendsAppointmentFragment.kt
+++ app/src/main/java/com/example/patient/RemovePatientattendsAppointmentFragment.kt
@@ -86,10 +86,10 @@ class RemovePatientattendsAppointmentFragment : Fragment(), View.OnClickListener
 
     override fun onItemSelected(parent: AdapterView<*>, v: View?, position: Int, id: Long) {
         if (parent === patientIdSpinner) {
-	            patientIdTextField.setText(allPatientpatientIds.get(position))
+	            patientIdTextField.setText(allPatientpatientIds[position])
 	        }
 	    if (parent ==appointmentIdSpinner) {
-	            appointmentIdTextField.setText(allAppointmentappointmentIds.get(position))
+	            appointmentIdTextField.setText(allAppointmentappointmentIds[position])
 	        }
     }
 
